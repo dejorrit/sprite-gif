206f410f568beeead17ff6c4f924dd0b12b0cc56
Jorrit Tinholt <dejorrit@gmail.com>
2018-06-21 23:35:58 +0200

    Demo

---
+++ demo/demo.js
@@ -0,0 +1,18 @@
+import SpriteGif from './../src/sprite-gif.js';
+
+let myGif       = document.querySelector('.js-my-gif');
+let playButton  = document.querySelector('.js-play');
+let pauseButton = document.querySelector('.js-pause');
+let stopButton  = document.querySelector('.js-stop');
+let speedInput  = document.querySelector('.js-speed');
+
+let mySprite = new SpriteGif(myGif, {
+  sprite: './sprite.png',
+});
+
+playButton.addEventListener('click',  () => mySprite.play());
+pauseButton.addEventListener('click', () => mySprite.pause());
+stopButton.addEventListener('click',  () => mySprite.stop());
+speedInput.addEventListener('change', (event) => {
+  mySprite.setSpeed(parseFloat(event.target.value));
+});
---
+++ demo/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const config = {
+	entry: {
+		'demo': './demo.js'
+	},
+};
+
+module.exports = config;
